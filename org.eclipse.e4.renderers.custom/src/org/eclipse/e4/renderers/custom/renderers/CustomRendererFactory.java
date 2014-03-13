d29343bae462be410d468f9b34d64046124ad996
Sopot Cela <sopotcela@gmail.com>
2014-03-13 23:06:12 +0100

    Added custom sash renderer with disabled dragging.

--- org.eclipse.e4.renderers.custom/src/org/eclipse/e4/renderers/custom/renderers/CustomRendererFactory.java
+++ org.eclipse.e4.renderers.custom/src/org/eclipse/e4/renderers/custom/renderers/CustomRendererFactory.java
@@ -2,14 +2,17 @@ package org.eclipse.e4.renderers.custom.renderers;
 
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.renderers.swt.SashRenderer;
 import org.eclipse.e4.ui.workbench.renderers.swt.WorkbenchRendererFactory;
 
 public class CustomRendererFactory extends WorkbenchRendererFactory {
 	
 	private AbstractPartRenderer stackRenderer;
 	private CustomWindowRenderer windowRenderer;
+	private CustomSashRenderer sashRenderer;
 
 	@Override
 	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
@@ -26,6 +29,14 @@ public class CustomRendererFactory extends WorkbenchRendererFactory {
 				windowRenderer = new CustomWindowRenderer();
 				initRenderer(windowRenderer);
 				return windowRenderer;
+			}
+		}
+		
+		if (uiElement instanceof MPartSashContainer){
+			if (sashRenderer == null){
+				sashRenderer = new CustomSashRenderer();
+				initRenderer(sashRenderer);
+				return sashRenderer;
 			}
 		}
 		return super.getRenderer(uiElement, parent);
---
+++ org.eclipse.e4.renderers.custom/src/org/eclipse/e4/renderers/custom/renderers/CustomSashRenderer.java
@@ -0,0 +1,22 @@
+package org.eclipse.e4.renderers.custom.renderers;
+
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
+import org.eclipse.e4.ui.workbench.renderers.swt.SashRenderer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+
+public class CustomSashRenderer extends SashRenderer{
+	
+	@Override
+	public Object createWidget(MUIElement element, Object parent) {
+		final Composite sashComposite = (Composite) super.createWidget(element, parent);
+		MPartSashContainer psc = (MPartSashContainer) element;
+		sashComposite.setLayout(psc.isHorizontal()?new FillLayout(SWT.HORIZONTAL):new FillLayout(SWT.VERTICAL));
+		return sashComposite;
+	}
+
+}
